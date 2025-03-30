63fd17074e4f1c23c89a81d5c7b174c60e4d5a8f
Bilal-dev <143352356+bilalvakani@users.noreply.github.com>
2025-03-30 15:20:08 +0500

    Update LoginPage.jsx

--- coffee/src/Components/LoginPage.jsx
+++ coffee/src/Components/LoginPage.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import LoginForm from '../Layout/LoginForm';  // LoginForm component ko import kar rahe hain
+import LoginForm from '../Layout/LoginForm';  
 
 const LoginPage = () => {
   return (
     <div className="min-h-screen bg-backgroundColor flex items-center justify-center">
-      <LoginForm />  {/* Login form ko yahaan render kar rahe hain */}
+      <LoginForm />  
     </div>
   );
 };
