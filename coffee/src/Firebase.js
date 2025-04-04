6447175a0837a09ef2403fc43598fb1f4ef33fad
Bilal-dev <143352356+bilalvakani@users.noreply.github.com>
2025-04-04 00:03:42 -0700

    Update Firebase.js

--- coffee/src/Firebase.js
+++ coffee/src/Firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC5KApwYAXFAZNeld9GF7XP865IFZ48WqM",
   authDomain: "coffee-shop-bbde8.firebaseapp.com",
@@ -15,7 +11,6 @@ const firebaseConfig = {
   appId: "1:662795675461:web:e2a6b7925312fab439402c"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
